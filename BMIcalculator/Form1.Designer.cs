ccd3b8c06a3363486b30cd16e351799f9166c396
rusley07 <ruslan.batura@yahoo.com.au>
2017-02-23 11:39:32 +1100

    Add files via upload

---
+++ BMIcalculator/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace BMIcalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.heightLabel = new System.Windows.Forms.Label();
+            this.weightTextBox = new System.Windows.Forms.TextBox();
+            this.weightLabel = new System.Windows.Forms.Label();
+            this.heightTextBox = new System.Windows.Forms.TextBox();
+            this.bodyLabel = new System.Windows.Forms.Label();
+            this.rangeLabel = new System.Windows.Forms.Label();
+            this.bodyMassLabel = new System.Windows.Forms.Label();
+            this.weightRangeLabel = new System.Windows.Forms.Label();
+            this.bodyMassButton = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // heightLabel
+            // 
+            this.heightLabel.AutoSize = true;
+            this.heightLabel.Location = new System.Drawing.Point(12, 28);
+            this.heightLabel.Name = "heightLabel";
+            this.heightLabel.Size = new System.Drawing.Size(38, 13);
+            this.heightLabel.TabIndex = 0;
+            this.heightLabel.Text = "Height";
+            // 
+            // weightTextBox
+            // 
+            this.weightTextBox.Location = new System.Drawing.Point(116, 52);
+            this.weightTextBox.Name = "weightTextBox";
+            this.weightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.weightTextBox.TabIndex = 1;
+            // 
+            // weightLabel
+            // 
+            this.weightLabel.AutoSize = true;
+            this.weightLabel.Location = new System.Drawing.Point(12, 55);
+            this.weightLabel.Name = "weightLabel";
+            this.weightLabel.Size = new System.Drawing.Size(41, 13);
+            this.weightLabel.TabIndex = 2;
+            this.weightLabel.Text = "Weight";
+            // 
+            // heightTextBox
+            // 
+            this.heightTextBox.Location = new System.Drawing.Point(116, 21);
+            this.heightTextBox.Name = "heightTextBox";
+            this.heightTextBox.Size = new System.Drawing.Size(100, 20);
+            this.heightTextBox.TabIndex = 3;
+            // 
+            // bodyLabel
+            // 
+            this.bodyLabel.AutoSize = true;
+            this.bodyLabel.Location = new System.Drawing.Point(12, 128);
+            this.bodyLabel.Name = "bodyLabel";
+            this.bodyLabel.Size = new System.Drawing.Size(88, 13);
+            this.bodyLabel.TabIndex = 4;
+            this.bodyLabel.Text = "Body Mass Index";
+            // 
+            // rangeLabel
+            // 
+            this.rangeLabel.AutoSize = true;
+            this.rangeLabel.Location = new System.Drawing.Point(12, 157);
+            this.rangeLabel.Name = "rangeLabel";
+            this.rangeLabel.Size = new System.Drawing.Size(76, 13);
+            this.rangeLabel.TabIndex = 5;
+            this.rangeLabel.Text = "Weight Range";
+            // 
+            // bodyMassLabel
+            // 
+            this.bodyMassLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.bodyMassLabel.Location = new System.Drawing.Point(165, 123);
+            this.bodyMassLabel.Name = "bodyMassLabel";
+            this.bodyMassLabel.Size = new System.Drawing.Size(100, 23);
+            this.bodyMassLabel.TabIndex = 6;
+            // 
+            // weightRangeLabel
+            // 
+            this.weightRangeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.weightRangeLabel.Location = new System.Drawing.Point(165, 157);
+            this.weightRangeLabel.Name = "weightRangeLabel";
+            this.weightRangeLabel.Size = new System.Drawing.Size(100, 23);
+            this.weightRangeLabel.TabIndex = 7;
+            // 
+            // bodyMassButton
+            // 
+            this.bodyMassButton.Location = new System.Drawing.Point(314, 99);
+            this.bodyMassButton.Name = "bodyMassButton";
+            this.bodyMassButton.Size = new System.Drawing.Size(75, 47);
+            this.bodyMassButton.TabIndex = 8;
+            this.bodyMassButton.Text = "Calculate Body Mass";
+            this.bodyMassButton.UseVisualStyleBackColor = true;
+            this.bodyMassButton.Click += new System.EventHandler(this.bodyMassButton_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.scoreLabel.Location = new System.Drawing.Point(165, 190);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(100, 23);
+            this.scoreLabel.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 293);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.bodyMassButton);
+            this.Controls.Add(this.weightRangeLabel);
+            this.Controls.Add(this.bodyMassLabel);
+            this.Controls.Add(this.rangeLabel);
+            this.Controls.Add(this.bodyLabel);
+            this.Controls.Add(this.heightTextBox);
+            this.Controls.Add(this.weightLabel);
+            this.Controls.Add(this.weightTextBox);
+            this.Controls.Add(this.heightLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label heightLabel;
+        private System.Windows.Forms.TextBox weightTextBox;
+        private System.Windows.Forms.Label weightLabel;
+        private System.Windows.Forms.TextBox heightTextBox;
+        private System.Windows.Forms.Label bodyLabel;
+        private System.Windows.Forms.Label rangeLabel;
+        private System.Windows.Forms.Label bodyMassLabel;
+        private System.Windows.Forms.Label weightRangeLabel;
+        private System.Windows.Forms.Button bodyMassButton;
+        private System.Windows.Forms.Label scoreLabel;
+    }
+}
+
---
+++ BMIcalculator/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BMIcalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bodyMassButton_Click(object sender, EventArgs e)
+        {
+            int Weight = 0;
+            double Height = 0;
+            double BMI = 0;
+            string weightRange = "";
+
+            if (weightTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter the weight", "weight");
+                weightTextBox.Focus();
+                return;
+            }
+            try
+            {
+                Weight = int.Parse(weightTextBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter the numbers only", "weight");
+                weightTextBox.SelectAll();
+                weightTextBox.Focus();
+                return;
+            }
+
+            if (heightTextBox.Text == "")
+            {
+                MessageBox.Show("Please enter the vehicle price", "Vehicle Price");
+                heightTextBox.Focus();
+                return;
+            }
+            try
+            {
+                Height = double.Parse(heightTextBox.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please enter the numbers only", "Vehicle Price");
+                heightTextBox.SelectAll();
+                heightTextBox.Focus();
+                return;
+            }
+
+            BMI = calcBMI(Weight, Height);
+
+            weightRange = calcWeightRange(BMI);
+
+            bodyMassLabel.Text = BMI.ToString("N2");
+            weightRangeLabel.Text = weightRange;
+
+        }
+
+        private double calcBMI(int Weight, double Height)
+        {
+            // method passed weight (kgs) and height (mtrs), calculates & returnes BMI
+            //double score = 0;
+
+            double BMI = 0;                      //declare a local variable to hold BMI
+            BMI = Weight / Math.Pow(Height, 2);  //calculate BMI using formula
+            return BMI;                          //return BMI
+        }
+
+        private string calcWeightRange(double bmi)
+        {
+            string range = "";
+            if (bmi < 18.5)
+            {
+                range = "Underweight";
+            }
+            if (bmi >= 18.5 && bmi < 24)
+            {
+                range = "Normal weight";
+            }
+            if (bmi >= 24 && bmi < 30)
+            {
+                range = "Overweight";
+            }
+            else
+            {
+                if (bmi >= 30)
+                {
+                    range = "Obese";
+                }
+            }
+
+           scoreLabel.Text = range.ToString();
+            return range;
+        }
+    }
+}
